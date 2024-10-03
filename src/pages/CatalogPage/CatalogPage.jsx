cbdcb2a7bb01d5589c40eda08802c5fadfc1aa1f
AlexxSich <alexxsich@gmail.com>
2024-10-02 22:56:06 -0300

    add vercel.json

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,19 +1,18 @@
-import { useEffect, useState } from "react";
-import css from "./CatalogPage.module.css";
-import { getTrucks } from "../../trucks-api";
-import CatalogList from "../../components/CatalogList/CatalogList";
+import { useEffect, useState } from 'react';
+import css from './CatalogPage.module.css';
+import { getTrucks } from '../../trucks-api';
+import CatalogList from '../../components/CatalogList/CatalogList';
 
 export default function CatalogPage() {
   const [trucks, setTrucks] = useState([]);
 
   useEffect(() => {
-    getTrucks().then((data) => setTrucks(data));
+    getTrucks().then(data => setTrucks(data));
   }, []);
   return (
     <div className={css.CatalogContainer}>
       <h2>Catalog</h2>
-      {/* {trucks.length > 0 && <CatalogList trucks={trucks} />} */}
-      <CatalogList trucks={trucks} />
+      {trucks.length > 0 && <CatalogList trucks={trucks} />}
     </div>
   );
 }
